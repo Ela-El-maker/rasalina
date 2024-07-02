08e0ff0e9bf7ca9e0e299b73df66e833d399ca6e
Ela-El-maker <feloela101@gmail.com>
2024-07-02 13:37:25 +0300

    Initial Commit -->> Integrating Admin Dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,22 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+// Admin All routes
+Route::controller(AdminController::class)->group(function(){
+    Route::get('/admin/logout', 'destroy')->name('admin.logout');
+});
+
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    // return view('dashboard');
+    return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
